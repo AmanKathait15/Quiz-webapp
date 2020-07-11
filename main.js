19cd8ec410765c1589953bcf63ba08cb6c8d5ce9
Aman Kathait <agnikathait171@gmail.com>
2020-07-11 20:21:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -15,84 +15,84 @@ const scoreDiv = document.getElementById("scoreContainer");
 let questions = [
     {
         question : "What does HTML stand for?",
-        imgSrc : "img/html.png",
-        choiceA : "Hypertext Markup Language",
+        imgSrc : "image_icon/html.png",
+        choiceA : "Hyper Text Markup Language",
         choiceB : "Hyper Text Makeup Language",
-        choiceC : "Hyphen Text Markup Language",
+        choiceC : "Hyphen Text Mark Language",
         correct : "A"
     },{
         question : "What does CSS stand for?",
-        imgSrc : "img/css.png",
+        imgSrc : "image_icon/css.png",
         choiceA : "Cascading Style Ship",
         choiceB : "Cascading Style Sheet",
         choiceC : "Cascading Sheet Style",
         correct : "B"
     },{
         question : "What does JS stand for?",
-        imgSrc : "img/js.png",
+        imgSrc : "image_icon/js.png",
         choiceA : "JavaShip",
         choiceB : "JavaSheet",
         choiceC : "JavaScript",
         correct : "C"
     },{
         question : "Which of the following attributes is used to open an hyperlink in new tab?",
-        imgSrc : "img/html.png",
+        imgSrc : "image_icon/html.png",
         choiceA : "tab",
         choiceB : "href",
         choiceC : "target",
         correct : "C"
     },{
         question : "Which of the following property is used to make a font italic or oblique?",
-        imgSrc : "img/css.png",
+        imgSrc : "image_icon/css.png",
         choiceA : "font-style",
         choiceB : "font-size",
         choiceC : "italic",
         correct : "A"
     },{
         question : "What is the HTML tag under which one can write the JavaScript code?",
-        imgSrc : "img/js.png",
+        imgSrc : "image_icon/js.png",
         choiceA : "JavaScript",
         choiceB : "script",
         choiceC : "js",
         correct : "B"
     },{
         question : "How many heading tags are there in HTML5?",
-        imgSrc : "img/html.png",
+        imgSrc : "image_icon/html.png",
         choiceA : " 2",
         choiceB : " 10",
         choiceC : " 6",
         correct : "C"
     },{
         question : " Which of the following property is used to set the height of an image?",
-        imgSrc : "img/css.png",
+        imgSrc : "image_icon/css.png",
         choiceA : "border",
         choiceB : "height",
         choiceC : "image",
         correct : "B"
     },{
         question : "Which of the following is not a reserved word in JavaScript?",
-        imgSrc : "img/js.png",
+        imgSrc : "image_icon/js.png",
         choiceA : "interface",
         choiceB : "throws",
         choiceC : "program",
         correct : "C"
     },{
         question : "What is the font-size of the h1 heading tag?",
-        imgSrc : "img/html.png",
+        imgSrc : "image_icon/html.png",
         choiceA : "3.5 em",
         choiceB : "2 em",
         choiceC : "1.5 em",
         correct : "B"
     },{
         question : "Which of the following defines a measurement in inches?",
-        imgSrc : "img/css.png",
+        imgSrc : "image_icon/css.png",
         choiceA : "mm",
         choiceB : "pt",
         choiceC : "in",
         correct : "C"
     },{
         question : "How can you add a comment in a JavaScript?",
-        imgSrc : "img/js.png",
+        imgSrc : "image_icon/js.png",
         choiceA : "//comment",
         choiceB : "#comment",
         choiceC : "^comment",
@@ -205,10 +205,10 @@ function scoreRender(){
     const scorePerCent = Math.round(100 * score/questions.length);
     
     // choose the image based on the scorePerCent
-    let img = (scorePerCent >= 80) ? "img/5.png" :
-              (scorePerCent >= 60) ? "img/4.png" :
-              (scorePerCent >= 40) ? "img/3.png" :
-              (scorePerCent >= 20) ? "img/2.png" :
+    let img = (scorePerCent >= 80) ? "image_icon/5.png" :
+              (scorePerCent >= 60) ? "image_icon/4.png" :
+              (scorePerCent >= 40) ? "image_icon/3.png" :
+              (scorePerCent >= 20) ? "image_icon/2.png" :
               "img/1.png";
     
     scoreDiv.innerHTML = "<img src="+ img +">";
